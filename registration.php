00f4a7a256a532e8d11940ab69c2eac59dedfbe0
stuhares <stu@blueprint3d.co.uk>
2026-08-04 22:34:56 +0100

    Initial commit: Barebone module structure

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * BluePrint3D Ltd
+ *
+ * @category  BluePrint3D
+ * @package   BluePrint3D_EtsyIntegration
+ * @author    BluePrint3D <support@blueprint3d.dev>
+ * @copyright Copyright (c) 2026 BluePrint3D Ltd
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BluePrint3D_EtsyIntegration',
+    __DIR__
+);
